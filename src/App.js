e982eb1e1abe05bc355f03da7cdd45b9e443e95c
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2022-05-18 17:17:51 +0800

    feature: upload file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import axios from "axios"
 
 function App() {
+  const [isUploading, setIsUploading] = useState(false)
+  const [file, setFile] = useState(null)  // the current file selected
+  const [uploadedFileURL, setUploadedFileURL] = useState(null)  // url of the uploaded file
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (!file) {
+      return
+    }
+
+    try {
+      setIsUploading(true)
+
+      // Get S3 signed url
+      const { data } = await axios.get("http://localhost:8000/s3-url", {
+        params: {
+          fileName: file.name,
+          fileType: file.type,
+        },
+      })
+
+      const { signedRequest, url } = data
+
+      // Upload to S3
+      await axios.put(signedRequest, file)
+
+      setUploadedFileURL(url)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setIsUploading(false)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleChange} />
+        <button type="submit">Upload</button>
+      </form>
+      <p>Uploading: {isUploading.toString()}</p>
+      {uploadedFileURL && <img src={uploadedFileURL} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
